3cbdf2f187b0188bc8563c3d538d86a38fa8d6a8
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-12 11:43:55 +0300

    task 1 Bus stop done

--- 02.AsynchronousProgramming/01.Bus-Stop/app.js
+++ 02.AsynchronousProgramming/01.Bus-Stop/app.js
@@ -1,3 +1,32 @@
 function getInfo() {
-    console.log("TODO...");
+
+    const stopId = document.getElementById('stopId').value;
+    const submit = document.getElementById('submit');
+    const stopName = document.getElementById('stopName');
+    const buses = document.getElementById('buses');
+
+    submit.addEventListener('click', () => {
+        console.log('clicked!')
+        const url = ' http://localhost:3030/jsonstore/bus/businfo/';
+        fetch(`${url}${stopId}`)
+            .then(res => {
+                return res.json();
+            })
+            .then(result => {
+                stopName.innerText = result.name;
+
+                let theBuses = Object.entries(result.buses);
+                theBuses.forEach(bus => {
+                    let li = document.createElement('li');
+                    li.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`
+                    buses.appendChild(li);
+                })
+
+                console.log(result)
+            })
+            .catch(err => {
+                stopName.textContent = 'Error'
+        })
+    })
+
 }
