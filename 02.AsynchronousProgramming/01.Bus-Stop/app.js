ea6027d6b884604315a03c641996e0774a792e70
da20shadow <92220876+da20shadow@users.noreply.github.com>
2022-07-12 12:11:45 +0300

    fixed the solution with better one ;d

--- 02.AsynchronousProgramming/01.Bus-Stop/app.js
+++ 02.AsynchronousProgramming/01.Bus-Stop/app.js
@@ -1,32 +1,30 @@
-function getInfo() {
+async function getInfo() {
 
     const stopId = document.getElementById('stopId').value;
-    const submit = document.getElementById('submit');
     const stopName = document.getElementById('stopName');
     const buses = document.getElementById('buses');
 
-    submit.addEventListener('click', () => {
-        console.log('clicked!')
-        const url = ' http://localhost:3030/jsonstore/bus/businfo/';
-        fetch(`${url}${stopId}`)
-            .then(res => {
-                return res.json();
-            })
-            .then(result => {
-                stopName.innerText = result.name;
+    console.log('clicked!')
+    const url = ' http://localhost:3030/jsonstore/bus/businfo/' + stopId;
 
-                let theBuses = Object.entries(result.buses);
-                theBuses.forEach(bus => {
-                    let li = document.createElement('li');
-                    li.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`
-                    buses.appendChild(li);
-                })
+    buses.innerHTML = '';
+    // buses.replaceChildren();
 
-                console.log(result)
-            })
-            .catch(err => {
-                stopName.textContent = 'Error'
+    try {
+        stopName.textContent = 'Loading...';
+        const response = await fetch(url);
+        const bussesInfo = await response.json();
+        stopName.textContent = bussesInfo.name;
+
+        let theBuses = Object.entries(bussesInfo.buses);
+        theBuses.forEach(bus => {
+            let li = document.createElement('li');
+            li.textContent = `Bus ${bus[0]} arrives in ${bus[1]} minutes`
+            buses.appendChild(li);
         })
-    })
+
+    } catch (err) {
+        stopName.textContent = 'Error';
+    }
 
 }
